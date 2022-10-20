eb25b372385586035eb349b871067a7d81640d93
“Southliu” <1275093225@qq.com>
2022-10-20 10:41:29 +0800

    fix: keepalive状态更新标题

--- src/hooks/useTitle.ts
+++ src/hooks/useTitle.ts
@@ -1,12 +1,19 @@
 import { useEffect } from 'react'
 import { TITLE_SUFFIX } from '@/utils/config'
+import { useActivate } from 'react-activation'
 
 /**
  * 标题
  */
 export function useTitle(title: string) {
+  const value = TITLE_SUFFIX + title ? `${TITLE_SUFFIX} - ${title}` : ''
+
   useEffect(() => {
-    const velue = title + title ? `${TITLE_SUFFIX} - ${title}` : ''
-    document.title = velue
+    document.title = value
   }, [title])
+  
+  // keepalive状态更新标题
+  useActivate(() => {
+    document.title = value
+  })
 }
