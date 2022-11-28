064ff248097faf501b6b2684a93efeb3b94b5846
southliu <1275093225@qq.com>
2022-11-28 14:37:39 +0800

    perf: antd语言

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,10 +5,9 @@ import App from './App'
 
 // antd
 import { theme, ConfigProvider } from 'antd'
-import zhCN from 'antd/locale/zh_CN'
+import zhCN from 'antd/es/locale/zh_CN'
 
 // antd主题
-import type { Locale } from 'antd/es/locale-provider'
 import type { RootState } from "@/stores"
 import { useSelector } from "react-redux"
 const { defaultAlgorithm, darkAlgorithm } = theme
@@ -27,15 +26,10 @@ function Page() {
     }
   }, [])
 
-  // 临时类型，为了解决打包之后语言包失效问题
-  interface ILocale extends Locale {
-    default: Locale;
-  }
-
   return (
     <Router>
       <ConfigProvider
-        locale={(zhCN as ILocale)?.default ?? zhCN}
+        locale={zhCN}
         theme={{
           algorithm: [theme === 'dark' ? darkAlgorithm : defaultAlgorithm]
         }}
