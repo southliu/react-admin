c36a5a4d315dff017430a299d6985d0463f802a5
South <1275093225@qq.com>
2022-12-06 11:22:26 +0800

    fix: 登录页useEffect

--- tests/login.spec.tsx
+++ tests/login.spec.tsx
@@ -1,16 +1,13 @@
 import { render, screen } from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
+import { describe, it } from 'vitest'
 import Wrapper from './Wrapper'
 import Login from '../src/pages/login'
 
 describe('Login component', () => {
   render(<Wrapper><Login /></Wrapper>)
 
-  it('should render', () => {
+  it('has a login button', () => {
+    render(<Wrapper><Login /></Wrapper>)
     screen.getByText(/登录/i)
-  })
-  
-  it('input', () => {
-    expect(true).toBeTruthy()
   })
 })
