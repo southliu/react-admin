be0f4ae610227feb1efc71e425d19e576bcc9e46
South <1275093225@qq.com>
2022-12-12 10:23:22 +0800

    perf: 首页没token跳转登录页

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useToken } from '@/hooks/useToken'
 import Image from '@/assets/images/welcome.png'
 
 function Page() {
+  const [getToken] = useToken()
+  const token = getToken()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!token) navigate('/login')
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [token])
+  
   return (
     <div className='flex justify-center'>
       <img
