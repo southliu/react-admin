23268f7dec78e1a1ff8033ac84b5dca99d49c793
South <1275093225@qq.com>
2022-12-15 11:47:37 +0800

    perf: 打包文件去除版本号

--- build/vite/build.ts
+++ build/vite/build.ts
@@ -11,15 +11,27 @@ export function buildOptions(): BuildOptions {
         chunkFileNames: 'assets/js/[name]-[hash].js',
         entryFileNames: 'assets/js/[name]-[hash].js',
         assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
+        manualChunks(name) {
+          if (name.includes('node_modules')) {
             // pnpm兼容
-            const pnpmName = id.includes('.pnpm') ? '.pnpm/' : ''
+            const pnpmName = name.includes('.pnpm') ? '.pnpm/' : ''
             const fileName = `node_modules/${pnpmName}`
 
-            return id
+            let result = name
               .split(fileName)[1]
               .split('/')[0]
+
+            if (result.includes('@')) {
+              const first = result.indexOf('@')
+              if (first > 0) {
+                result = result.substring(0, first)
+              } else {
+                const second = result.indexOf('@', 1)
+                result = result.substring(0, second)
+              }
+            }
+
+            return result
           }
         }
       }
