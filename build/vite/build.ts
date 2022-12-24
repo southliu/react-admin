f836ba2be75763f917fca0bb85a9bccbf2378460
southliu <1275093225@qq.com>
2022-12-24 17:40:10 +0800

    perf: 分包

--- build/vite/build.ts
+++ build/vite/build.ts
@@ -33,6 +33,43 @@ export function buildOptions(): BuildOptions {
 
             return result
           }
+
+          // 公共组件
+          if (id.includes('/src/components/')) {
+            const fileName = '/src/components/'
+
+            const result = id
+              .split(fileName)[1]
+              .split('/')[0]
+
+            return `components_${result}`
+          }
+
+          // 页面分包
+          if (id.includes('/src/pages/')) {
+            const fileName = '/src/pages/'
+
+            const file = id.split(fileName)[1]
+            const categorize = file?.split('/')?.[0] || ''
+            let result = file?.split('/')?.[1] || 'index'
+
+            if (result.includes('/')) result = result?.split('/')[0] || ''
+            if (result.includes('.tsx')) result = result.substring(0, result.length - 4)
+
+            // 组件
+            if (result === 'components' || result === 'component') {
+              let compName = '/components/'
+              if (id.includes('/component/')) compName = '/component/'
+
+              let comResult = id.split(compName)[1]
+              if (comResult.includes('/')) comResult = comResult?.split('/')[0] || ''
+              if (comResult.includes('.tsx')) comResult = comResult.substring(0, comResult.length - 4)
+
+              return `page_${categorize}_comp_${comResult}`
+            }
+
+            return `page_${categorize}_${result}`
+          }
         }
       }
     }
