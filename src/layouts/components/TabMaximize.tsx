4dd0be059fe1771366f12ea8fdc0a7bbb69178e0
southliu <1275093225@qq.com>
2023-06-11 23:19:38 +0800

    style: 内容页最大化

--- src/layouts/components/TabMaximize.tsx
+++ src/layouts/components/TabMaximize.tsx
@@ -1,5 +1,4 @@
 import type { AppDispatch } from '@/stores';
-import { Tooltip } from 'antd';
 import { Icon } from '@iconify/react';
 import { useDispatch } from 'react-redux';
 import { toggleMaximize } from '@/stores/tabs';
@@ -16,22 +15,17 @@ function TabMaximize() {
   };
 
   return (
-    <Tooltip
-      title={ isMaximize ? "最小化" : "最大化"}
-      placement="bottom"
-    >
-      <Icon
-        className={`
-          flex
-          items-center
-          justify-center
-          text-lg
-          cursor-pointer
-        `}
-        icon={ isMaximize ? "ant-design:compress-outlined" : "ant-design:expand-outlined" }
-        onClick={onClick}
-      />
-    </Tooltip>
+    <Icon
+      className={`
+        flex
+        items-center
+        justify-center
+        text-lg
+        cursor-pointer
+      `}
+      icon={ isMaximize ? "ant-design:compress-outlined" : "ant-design:expand-outlined" }
+      onClick={onClick}
+    />
   );
 }
 
