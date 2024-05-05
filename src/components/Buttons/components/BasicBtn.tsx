6a407030379732bf9a3920bd48cb72b7b76e575a
southliu <1275093225@qq.com>
2024-05-05 22:48:02 +0800

    feat: 基础按钮

---
+++ src/components/Buttons/components/BasicBtn.tsx
@@ -0,0 +1,27 @@
+import type { ButtonProps } from 'antd';
+import { Button } from 'antd';
+
+interface Props extends Omit<ButtonProps, 'loading'> {
+  isLoading: boolean;
+  children?: JSX.Element | string;
+}
+
+function UpdateBtn(props: Props) {
+  const { isLoading, children } = props;
+
+  // 清除自定义属性
+  const params: Partial<Props> = { ...props };
+  delete params.isLoading;
+
+  return (
+    <Button
+      type='primary'
+      {...params}
+      loading={!!isLoading}
+    >
+      { children }
+    </Button>
+  );
+}
+
+export default UpdateBtn;
--- src/components/Buttons/index.ts
+++ src/components/Buttons/index.ts
@@ -1,2 +1,3 @@
+export { default as BasicBtn } from './components/BasicBtn';
 export { default as UpdateBtn } from './components/UpdateBtn';
 export { default as DeleteBtn } from './components/DeleteBtn';
