cf1f6a8eaf5fd17bc91c3c6c4ef1d11105e98b9d
southliu <1275093225@qq.com>
2024-08-24 15:43:39 +0800

    perf: 动态路由参数页面

--- src/pages/demo/[id]/dynamic/index.tsx
+++ src/pages/demo/[id]/dynamic/index.tsx
@@ -1,10 +1,14 @@
+import BasicCard from '@/components/Card/BasicCard';
 import { useParams } from 'react-router-dom';
 
 function Dynamic() {
   const { id } = useParams();
 
   return (
-    <div>id: { id }</div>
+    <BasicCard className='mt-10px mx-5px'>
+      <div>/demo/123/dynamic中的123为动态参数，可自由修改，文件路径为：/demo/[id]/dynamic。</div>
+      <div>id: <span className='font-bold'>{ id }</span></div>
+    </BasicCard>
   );
 }
 
