44fb91aad402c95c87e9242fd1820b6835ed4ac2
South <1275093225@qq.com>
2025-07-21 15:15:10 +0800

    Update VirtualTable.tsx

--- src/pages/demo/virtualScroll/components/VirtualTable.tsx
+++ src/pages/demo/virtualScroll/components/VirtualTable.tsx
@@ -4,6 +4,7 @@ import BaseTable from '@/components/Table/BaseTable';
 
 function VirtualTable() {
   const { t } = useTranslation();
+  const [tableData, setTableData] = useState<object[]>([]);
 
   const columns: TableColumn[] = [
     { title: 'ID', dataIndex: 'id', width: 200 },
@@ -12,19 +13,22 @@ function VirtualTable() {
     { title: t('system.age'), dataIndex: 'number', width: 200 },
   ];
 
-  const data = new Array(0).fill({});
-  for (let i = 0; i < 10000; i++) {
-    const num = i + 1;
-    data.push({
-      id: num,
-      name: 'name' + num,
-      phone: num * 13,
-      number: num * 3,
-    });
-  }
+  useEffect(() => {
+    const data = new Array(0).fill({});
+    for (let i = 0; i < 10000; i++) {
+      const num = i + 1;
+      data.push({
+        id: num,
+        name: 'name' + num,
+        phone: num * 13,
+        number: num * 3,
+      });
+    }
+    setTableData(data);
+  }, []);
 
   return (
-    <BaseTable isVirtual columns={columns} dataSource={data} scrollY={500} isOperate={false} />
+    <BaseTable isVirtual columns={columns} dataSource={tableData} scrollY={500} isOperate={false} />
   );
 }
 
